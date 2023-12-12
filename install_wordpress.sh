f2e9eef3af587f2bbbc704e16c3160b345a80bc8
AhnafMuttaki <ahnafx88@gmail.com>
2023-12-12 19:09:05 +0600

    added load script protection.xmlrpc protection.ram usage.cron disable and db integration

--- install_wordpress.sh
+++ install_wordpress.sh
@@ -3,21 +3,25 @@
 # OS Compatibility: Amazon Linux 2
 # Change #DB_PASS before execution
 
+database_host = localhost
+database_port = 3306
+database_name = wordpress_db
+database_username = sample_user
+database_password = sample_password_2023!
+wordpress_site_url = http://localhost/wordpress
+
 sudo yum update -y
-
-# php setup and library install
-sudo amazon-linux-extras install nginx1 php8.1 -y
+sudo amazon-linux-extras install nginx1 php7.4 -y
 sudo yum clean metadata
-sudo yum install git php-{pear,cgi,common,curl,mbstring,gd,mysqli,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip} -y
-sudo yum install php-{pgsql,xml,soap,sodium,opcache} -y
+sudo yum install git php-{pear,cgi,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip} -y
 
-# Mariadb Server
-# curl -LsS -O https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
-# sudo bash mariadb_repo_setup --os-type=rhel  --os-version=7 --mariadb-server-version=10.9
-# sudo rm -rf /var/cache/yum
-# sudo yum makecache
-# sudo amazon-linux-extras install epel -y
-# sudo yum install MariaDB-server MariaDB-client -y
+Mariadb Server
+curl -LsS -O https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
+sudo bash mariadb_repo_setup --os-type=rhel  --os-version=7 --mariadb-server-version=10.9
+sudo rm -rf /var/cache/yum
+sudo yum makecache
+sudo amazon-linux-extras install epel -y
+sudo yum install MariaDB-server MariaDB-client -y
 
 
 # Back up existing config
@@ -37,6 +41,7 @@ sudo sed -i 's|;*post_max_size = 8M|post_max_size = 600M|g' /etc/php.ini
 sudo sed -i 's|;*upload_max_filesize = 2M|upload_max_filesize = 600M|g' /etc/php.ini
 sudo sed -i 's|;*max_file_uploads = 20|max_file_uploads = 20|g' /etc/php.ini
 # nginx.conf
+
 cat << EOF > /etc/nginx/nginx.conf
 # For more information on configuration, see:
 #   * Official English Documentation: http://nginx.org/en/docs/
@@ -76,6 +81,12 @@ http {
         add_header X-Content-Type-Options "nosniff";
         index index.php index.html index.htm;
         charset utf-8;
+        location ~ \/wp-admin\/load-(scripts|styles).php { 
+            deny all; 
+        }
+        location = /xmlrpc.php {
+            deny all;
+        }
         location / {
             try_files \$uri \$uri/ /index.php?\$query_string;
         }
@@ -106,11 +117,126 @@ for i in nginx php-fpm mariadb; do sudo systemctl start $i; done
 #sudo systemctl restart nginx.service
 
 # # Create DB and user
-# sudo mysql -e "CREATE USER IF NOT EXISTS 'moodle_user'@'localhost' IDENTIFIED BY '#DB_PASS'"
-# sudo mysql -e "GRANT ALL PRIVILEGES ON moodledb.* to 'moodle_user'@'localhost'"
+sudo mysql -e "CREATE USER IF NOT EXISTS '$database_username'@'$database_host' IDENTIFIED BY '$database_password'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON $database_name.* to '$database_user'@'$database_host'"
 
 # Download Wordpress
 wget https://wordpress.org/latest.zip
 sudo unzip latest.zip
-sudo cp -r wordpress /usr/share/nginx/html
-sudo cp /usr/share/nginx/html/wordpress/wp-config-sample.php /usr/share/nginx/html/wordpress/wp-config.php
+sudo cp -r wordpress /usr/share/nginx/html/wordpress
+
+
+
+cat << EOF > /usr/share/nginx/html/wordpress/wp-config.php
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the website, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/documentation/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', '#DB_NAME' );
+
+/** Database username */
+define( 'DB_USER', '#DB_USER' );
+
+/** Database password */
+define( 'DB_PASSWORD', '#DB_PASSWORD' );
+
+/** Database hostname */
+define( 'DB_HOST', '#DB_HOST' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+define( 'CONCATENATE_SCRIPTS', false );
+define('DISABLE_WP_CRON', true);
+define('WP_MEMORY_LIMIT', '256M');
+define('FS_METHOD', 'direct');
+define( 'WP_HOME', '#DB_SITE_HOME' );
+define( 'WP_SITEURL', '#DB_SITE_URL' );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
+EOF
+
+sudo sed -i "s|#DB_USER|$database_username|g" /usr/share/nginx/html/wordpress/wp-config.php
+sudo sed -i "s|#DB_PASSWORD|$database_password|g" /usr/share/nginx/html/wordpress/wp-config.php
+sudo sed -i "s|#DB_NAME|$database_name|g" /usr/share/nginx/html/wordpress/wp-config.php
+sudo sed -i "s|#DB_HOST|$database_host|g" /usr/share/nginx/html/wordpress/wp-config.php
+sudo sed -i "s|#DB_SITE_HOME|$wordpress_site_url|g" /usr/share/nginx/html/wordpress/wp-config.php
+sudo sed -i "s|#DB_SITE_URL|$wordpress_site_url|g" /usr/share/nginx/html/wordpress/wp-config.php
+
+
+
+# sudo cp /usr/share/nginx/html/wordpress/wp-config-sample.php /usr/share/nginx/html/wordpress/wp-config.php
